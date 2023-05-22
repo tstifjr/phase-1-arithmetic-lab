81f08e8d8b7eed2b46d3e5e77df552b13fb3ed23
TJ Stifter <tjstifter@gmail.com>
2023-05-22 12:08:52 -0400

    completes lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let num3 = 19;
+let num4 = 5;
+let set = (1, 10, 15, 20);
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random()*100)+1;
+
+const mod = num3 % num4;
+
+const max = Math.max(5, 10, 15, 20);
